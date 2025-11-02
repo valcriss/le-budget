62313e037f749932ef94d997ffb49c8d856f6353
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-11-03 00:29:00 +0100

    refactor: simplify test environment setup by removing HTTP client provider

--- frontend/src/test-setup.ts
+++ frontend/src/test-setup.ts
@@ -1,7 +1,4 @@
 import '@angular/compiler';
-import { provideHttpClient } from '@angular/common/http';
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
-setupZoneTestEnv({
-  providers: [provideHttpClient()],
-});
+setupZoneTestEnv();
