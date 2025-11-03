14c6b1994555df05cbe3ea92c120e985bee012b2
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-11-03 18:49:24 +0100

    feat: update budget category summary to use dynamic data binding for category details

--- frontend/src/app/components/budget/budget-category-summary/budget-category-summary.ts
+++ frontend/src/app/components/budget/budget-category-summary/budget-category-summary.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { formatCurrencyWithSign, getAmountClass } from '../../../shared/formatters';
+import { Category } from '../../../core/categories/categories.models';
+import { BudgetCategory } from '../../../core/budget/budget.models';
 
 @Component({
   selector: 'app-budget-category-summary',
@@ -16,9 +18,14 @@ export class BudgetCategorySummary {
 
   @Input() neededThisMonth: number = 0
   @Input() neededForNextMonths: number = 0
+  @Input() budgetCategory: BudgetCategory | null | undefined = null
 
   formatCurrencyWithSign(value?: string | number) {
     return formatCurrencyWithSign(value, false);
+  }
+
+  getAvailableFromPreviousMonth() {
+    return (this.budgetCategory?.available ?? 0) - (this.budgetCategory?.assigned ?? 0) - (this.budgetCategory?.activity ?? 0);
   }
 
   getAmountClass(value?: string | number) {
